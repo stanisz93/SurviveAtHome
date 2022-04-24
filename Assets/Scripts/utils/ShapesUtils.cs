4e6a10aaecb9cfb0f3053b2a260e1cbc01679bf9
Jakub <stanisz1993@gmail.com>
2022-04-24 19:26:40 +0200

    Small fixes

---
+++ Assets/Scripts/utils/ShapesUtils.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using UnityEngine;
+
+public class ShapeUtils
+{
+
+public static void DrawLine(Vector3 start, Vector3 end, Color color)
+    {
+       Debug.DrawLine(start, end, color);
+    }
+
+}
